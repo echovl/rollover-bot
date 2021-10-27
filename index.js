8e8f77143e2abaa5726252e0d18171b7cadf3ca9
Alonso Villegas <alonsovillegas816@gmail.com>
2021-10-26 22:26:19 -0500

    add swap functionality

--- index.js
+++ index.js
@@ -2,64 +2,152 @@ import Web3 from "web3"
 import { readFileSync } from "fs"
 import pino from "pino"
 
-const logger = pino()
 const PROVIDER_URL = "https://rpc.ftm.tools/"
 const SUMMIT_CONTRACT_ADDRESS = "0x46d303b6829aDc7AC3217D92f71B1DbbE77eBBA2"
-const SUMMIT_CONTRACT_ABI = JSON.parse(readFileSync("./abi.json"))
+const SUMMIT_CONTRACT_ABI = JSON.parse(readFileSync("./summit_abi.json"))
+const SUMMIT_TOKEN_ABI = JSON.parse(readFileSync("./summit_token_abi.json"))
+const SUMMIT_TOKEN_ADDRESS = "0x8F9bCCB6Dd999148Da1808aC290F2274b13D7994"
+const SPOOKY_ROUTER_ADDRESS = "0xf491e7b69e4244ad4002bc14e878a34207e38c29"
+const SPOOKY_ROUTER_ABI = JSON.parse(readFileSync("./spooky_abi.json"))
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-async function run() {
-    const web3 = new Web3(PROVIDER_URL)
+const SUMMIT = "0x8f9bccb6dd999148da1808ac290f2274b13d7994"
+const WFTM = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83"
 
-    const contract = new web3.eth.Contract(
-        SUMMIT_CONTRACT_ABI,
-        SUMMIT_CONTRACT_ADDRESS
-    )
+const web3 = new Web3(PROVIDER_URL)
+const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
 
-    const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
-    web3.eth.accounts.wallet.add(account)
+// Add our account to the wallet to be able to use contracts
+web3.eth.accounts.wallet.add(account)
 
-    const balance = web3.utils.fromWei(
-        await web3.eth.getBalance(account.address)
-    )
+const rolloverContract = new web3.eth.Contract(
+    SUMMIT_CONTRACT_ABI,
+    SUMMIT_CONTRACT_ADDRESS
+)
+const summitContract = new web3.eth.Contract(
+    SUMMIT_TOKEN_ABI,
+    SUMMIT_TOKEN_ADDRESS
+)
+const routerContract = new web3.eth.Contract(
+    SPOOKY_ROUTER_ABI,
+    SPOOKY_ROUTER_ADDRESS
+)
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function start() {
+    if (!process.argv[2]) {
+        throw new Error("Please specify the rollover position")
+    }
+
+    const position = parseInt(process.argv[2])
+    const logger = pino(`rollover_${position}.log`)
+
+    logger.info("Starting rollover bot")
 
     while (true) {
         try {
-            logger.info(`Balance: ${balance} FTM`)
-
-            const roundEndTimestamp = await contract.methods
-                .roundEndTimestamp(1)
+            // Get rollover event timestamp
+            const roundEndTimestamp = await rolloverContract.methods
+                .roundEndTimestamp(position)
                 .call()
 
             const block = await web3.eth.getBlock("latest")
-            const gasPrice = await web3.eth.getGasPrice()
             const timestamp = Date.now() / 1000
 
+            // Sleep until 250 seconds before the rollover
+            if (roundEndTimestamp - timestamp > 500) {
+                logger.info("Sleeping")
+                await sleep((roundEndTimestamp - timestamp - 250) * 1000)
+                continue
+            }
+
+            if (timestamp < roundEndTimestamp) continue
+
+            logger.info(`Rollover ${position} available`)
             logger.info(`Block ${block.number}, Timestamp: ${block?.timestamp}`)
             logger.info(`Machine timestamp: ${Date.now() / 1000}`)
             logger.info(`RoundEndTimestamp: ${roundEndTimestamp}`)
-            logger.info(`Gas price: ${web3.utils.fromWei(gasPrice, "gwei")}`)
 
-            if (timestamp >= roundEndTimestamp) {
-                logger.info("Rollover available ...")
+            // Execute the rollover and claim the rewards
+            const receipt = await claimRolloverRewards(position)
 
-                const gasAmount = await contract.methods
-                    .rollover(1)
-                    .estimateGas({ from: account.address, gas: 10000000 })
-
-                const receipt = await contract.methods.rollover(1).send({
-                    from: account.address,
-                    gasPrice,
-                    gas: Math.round(gasAmount * 1.5),
-                })
-
-                logger.info(`Tx hash: ${receipt}`)
-            } else {
+            if (receipt.transactionHash) {
+                await sleep(250000)
+                await swapRewardsForFTM()
             }
         } catch (err) {
-            logger.error(`Error: ${err.message}`)
+            logger.error(err.message)
         }
     }
 }
 
-run()
+async function claimRolloverRewards(position) {
+    const gasPrice = await web3.eth.getGasPrice()
+
+    // Estime gas required for rollover
+    const gasAmount = await rolloverContract.methods
+        .rollover(position)
+        .estimateGas({ from: account.address, gas: 10000000 })
+
+    const receipt = await rolloverContract.methods.rollover(position).send({
+        from: account.address,
+        gasPrice,
+        gas: Math.round(gasAmount * 1.5),
+    })
+
+    logger.info(`Claimed rollover rewards, tx hash: ${receipt.transactionHash}`)
+
+    return receipt
+}
+
+async function swapRewardsForFTM() {
+    // Swap parameters
+    const slippage = 0.01
+    const deadline = Math.round(Date.now() / 1000 + 100)
+    const path = [SUMMIT, WFTM]
+
+    // Get summit rewards balance
+    const summitBalance = await summitContract.methods
+        .balanceOf(account.address)
+        .call()
+
+    const [_, ftmAmount] = await routerContract.methods
+        .getAmountsOut(summitBalance, path)
+        .call()
+
+    const minFTMAmount = web3.utils.toBN(
+        Math.round(ftmAmount - ftmAmount * slippage)
+    )
+
+    const gasPrice = await web3.eth.getGasPrice()
+    const gasAmount = await routerContract.methods
+        .swapExactTokensForETH(
+            summitBalance,
+            minFTMAmount,
+            path,
+            account.address,
+            deadline
+        )
+        .estimateGas({ from: account.address, gas: 10000000 })
+
+    const receipt = await routerContract.methods
+        .swapExactTokensForETH(
+            summitBalance,
+            minFTMAmount,
+            path,
+            account.address,
+            deadline
+        )
+        .send({
+            from: account.address,
+            gasPrice,
+            gas: Math.round(gasAmount * 1.5),
+        })
+
+    logger.info(`Swapped SUMMIT for FTM, tx hash: ${receipt.transactionHash}`)
+}
+
+start()
